ae2313903c48ba742309e683396793a2062bf67c
Michael <michaelquint@gmail.com>
2026-06-15 19:42:25 -0400

    fs stuff

--- app3.js
+++ app3.js
@@ -7,3 +7,6 @@ console.log(calc.add(4, 8));
 console.log(calc.subtract(9, 8));
 console.log(calc.multiply(4, 5));
 console.log(calc.divide(30, 5));
+
+const fs = require("fs");
+fs.writeFileSync("notes.txt", "Hello World");
